23c2975c3442c261c271e1b64ec584b0768ad164
Michael Vilim <michael.vilim@gmail.com>
2019-10-29 18:44:45 -0500

    Expose version from cpp extension

--- python/bamboo/__init__.py
+++ python/bamboo/__init__.py
@@ -1,2 +1,4 @@
 from bamboo.nodes import FlattenStrategy, NameStrategy, JoinType
 from bamboo.core import from_object, from_arrow, from_avro, from_json, from_pbd
+
+from bamboo_cpp_bind import __version__
