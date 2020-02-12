2bc24abd59d440b0d5d798cfd9fa870acb1d6fbd
Michael Vilim <michael.vilim@gmail.com>
2020-02-11 20:32:56 -0600

    Use manylinux2014 for linux builds

--- scripts/travis_build.sh
+++ scripts/travis_build.sh
@@ -1,6 +1,7 @@
 set -e -x
 
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+    ./scripts/docker/install_proto.sh
     pip install cibuildwheel
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
     HOMEBREW_NO_AUTO_UPDATE=1 brew install protobuf
